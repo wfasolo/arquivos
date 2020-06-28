8806efee71c1fdc84fd0762171a5b99993d4749c
wfasolo <wfasolo@gmail.com>
2020-06-28 20:02:06 -0300

    a

--- dados/test.py
+++ dados/test.py
@@ -1,18 +1,17 @@
+from sklearn.model_selection import train_test_split
 import pandas as pd
-import numpy as np 
+import numpy as np
 import tensorflow as tf
 import matplotlib as plt
 import sklearn
-df=pd.read_csv('a.csv')
+df = pd.read_csv('a.csv')
 print(df)
 
 print(df.corr())
-df['tipo']=df['tipo'].map({True:1,False:0})
-print(len(df.loc[df['tipo']==1]))
+df['tipo'] = df['tipo'].map({True: 1, False: 0})
+print(len(df.loc[df['tipo'] == 1]))
 
-from sklearn.model_selection import train_test_split
-X=df.drop(columns=('valor'))
-X=X.drop(columns=('tipo')).values
-y=df['valor'].values
-xtreino,ytreino,xteste,yteste=train_test_split(X,y, test_size=.1)
-
+X = df.drop(columns=('valor'))
+X = X.drop(columns=('tipo')).values
+y = df['valor'].values
+xtreino, ytreino, xteste, yteste = train_test_split(X, y, test_size=.1)
