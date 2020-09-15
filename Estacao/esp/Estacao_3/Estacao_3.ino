4492cb17ef936aff6a10e029cc80c8fc7ab8aca2
wfasolo <wfasolo@gmail.com>
2020-09-15 09:14:20 -0300

    loterias

--- Estacao/esp/Estacao_3/Estacao_3.ino
+++ Estacao/esp/Estacao_3/Estacao_3.ino
@@ -17,12 +17,20 @@ NTPClient ntp(udp, "a.st1.ntp.br", -3 * 3600, 60000);
 Adafruit_BME280 bme;
 
 unsigned long cont = -55,
-              cont2 = 0;
+              cont2 = 0,
+              cont3 = -4,
+              cont4 = 0;
 
 float     pres = 0,
           temp = 0,
-          umid = 0;
-int chuv = 0;
+          umid = 0,
+          pres2 = 0,
+          temp2 = 0,
+          umid2 = 0;
+
+int chuv = 0,
+    id = 0;
+
 const int pinoSensor = D8;
 
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
@@ -46,18 +54,56 @@ void setup()
 
 void loop()
 {
+  //leitura
+  if ((millis() / 1000) - cont3 >= 5)
+  {
+
+    // medicao da temperatura
+    pres2 = pres2 + bme.readPressure();
+    temp2 = temp2 + bme.readTemperature();
+    umid2 = umid2 + bme.readHumidity();
+    cont4++;
+    //
+
+    // chuva
+    ler_chuva();
+    //
+
+    //chuva tempo real
+    if (id == 0 && chuv == 1) {
+      thing.stream(thing["parametros"]);
+      delay(100);
+      yield();
+
+      ler_chuva();
+
+      if (chuv == 1) {
+        pres = pres2 / (cont4 * 100 * 0.99),
+        temp = temp2 / cont4,
+        umid = umid2 / (cont4 * 0.9);
+
+        thing.write_bucket("dados_estacao1", "parametros");
+      }
+      id = 1;
+    }
+    //
+
+    cont3 = (millis() / 1000);
+  }
+  //
+
+  //verificação
   if ((millis() / 1000) - cont >= 59.5)
   {
     //verifica a conexao WiFi
     if (WiFi.status() != WL_CONNECTED)
     {
       WiFi.reconnect();
-      cont = (millis() / 1000) - 59;
       yield();
+      cont = (millis() / 1000) - 59;
     }
 
     else
-
     {
       // Atualizacao da hora
       ntp.forceUpdate();
@@ -74,51 +120,36 @@ void loop()
       }
       //
 
-      // chuva
-      if (digitalRead(pinoSensor) == HIGH)
+      // gravacao no banco de dados a cada 20 minutos
+      if (minuto == 0 || minuto == 20 || minuto == 40)
       {
-        chuv = 0;
-      }
-      else
-      {
-        chuv = 1;
-      }
-      //
+        pres = pres2 / (cont4 * 100 * 0.99),
+        temp = temp2 / cont4,
+        umid = umid2 / (cont4 * 0.9);
+        ler_chuva();
 
-      // medicao da temperatura
-      pres = 0;
-      temp = 0;
-      umid = 0;
-
-      for (int i = 0; i <= 99; i++)
-      {
-
-        pres = pres + bme.readPressure();
-        temp = temp + bme.readTemperature();
-        umid = umid + bme.readHumidity();
-        delay(10);
-      }
-
-      pres = pres / (10000 * 0.99),
-      temp = temp / 100,
-      umid = umid / (100 * 0.9);
-      //
-
-      // gravacao no banco de dados a cada 15 minutos
-      if (minuto == 00 || minuto == 20 || minuto == 40)
-      {
         thing.write_bucket("dados_estacao1", "parametros");
+
+        pres2 = 0;
+        temp2 = 0;
+        umid2 = 0;
+        cont4 = 0;
       }
       //
+
+      pres = bme.readPressure() / (100 * 0.99);
+      temp = bme.readTemperature();
+      umid = bme.readHumidity() / 0.9;
 
       // enviar dados
       thing.stream(thing["parametros"]);
       //thing.stream(thing["Alt"]);
 
       cont = (millis() / 1000);
-      yield();
+
     }
   }
+  //
 
   if ((millis() / 1000) - cont2 >= 15)
   {
@@ -127,3 +158,18 @@ void loop()
     yield();
   }
 }
+
+// funcao ler chuva
+void ler_chuva() {
+
+  if ( digitalRead(pinoSensor) == HIGH)
+  {
+    chuv = 0;
+    id = 0;
+  }
+  else
+  {
+    chuv = 1;
+  }
+
+}//
