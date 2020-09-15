e9584e5369cb858b119140d62146b71decd950aa
wfasolo <wfasolo@gmail.com>
2020-09-15 09:27:02 -0300

    1

---
+++ loterias/mega.py
@@ -0,0 +1,71 @@
+import io
+import zipfile
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+
+class Mega:
+
+    def __init__(self):
+
+        self.y_train = []
+        self.X_train = []
+        self.ultimo = []
+        self.previsao1 = []
+
+    def prepara(self):
+
+        url = 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip'
+
+        response = requests.get(url, stream=True)
+        mega_zip = zipfile.ZipFile(io.BytesIO(response.content))
+        mega_html = mega_zip.open('d_mega.htm')
+        mega_pd = pd.read_html(mega_html)
+        mega_list = mega_pd[0]
+        mega_list.drop_duplicates(
+            subset='Concurso', keep='first', inplace=True)
+
+        megafacil = pd.DataFrame([mega_list['1ª Dezena'], mega_list['2ª Dezena'], mega_list['3ª Dezena'],
+                                  mega_list['4ª Dezena'], mega_list['5ª Dezena'], mega_list['6ª Dezena']]).T
+
+        (self.X_train, self.y_train) = (
+            megafacil[:-1].values, megafacil[1:].values)
+        self.ultimo = self.y_train[-1:]
+
+        return self.X_train, self.y_train, self.ultimo
+
+    def model(self, n_n=1):
+
+        # Tainan model
+        model1 = KNeighborsClassifier(n_neighbors=(n_n))
+        model1.fit(self.X_train, self.y_train)
+
+        # Fazer previsoes
+        previsao = model1.predict(self.ultimo)
+        self.previsao1 = previsao[0]
+        return self.previsao1
+
+
+x = 100
+lt = Mega()
+lt.prepara()
+previsao2 = []
+print('Previsão :', set(lt.model()))
+
+for i in range(1, 1000):
+    previsao = lt.model(i)
+    previsao2.extend(previsao)
+
+    if x == i:
+
+        previsao3 = pd.Series(previsao2)
+        print('Prev', x, ':', set(pd.DataFrame(
+            previsao3.value_counts()[:6], columns=['Propabilidade']).T))
+        x += 250
+
+# bolas = bolas.append(pd.Series(previsao1[0], index=bolas.columns), ignore_index=True)
