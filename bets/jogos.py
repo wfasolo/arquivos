6be2d7d9c48875258c38ad5f9584ee2f9fcd89eb
wfasolo <wfasolo@gmail.com>
2020-11-02 22:16:57 -0300

    casa

--- bets/jogos.py
+++ bets/jogos.py
@@ -1,3 +1,5 @@
+https://rapidapi.com/api-sports/api/api-football/pricing
+
 import requests
 import pandas as pd
 key = '&APIkey=6a6e60821e72b841ab0b6ae4f09e9379b3b3a48fb72237a6ee1f2eb51c7924e7'
@@ -6,6 +8,7 @@ time1 = '&firstTeam=Internacional'
 time2 = '&secondTeam=Flamengo RJ'
 
 url = 'https://apiv2.apifootball.com/?action=get_events&from=2020-10-13&to=2020-11-02&timezone=America/Sao_Paulo&league_id=68'+key
+         https://apifootball.com/api/?action=get_events&from=2016-10-30&to=2016-11-01&league_id=62&APIkey=xxxxxxxxxxxxxx
 
 r = requests.get(url)
 
