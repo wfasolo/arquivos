a822b82201f97d26caadb44fc84daca544c946b4
wfasolo <wfasolo@gmail.com>
2021-01-18 09:31:55 -0300

    casa

--- jogos/lixo.py
+++ jogos/lixo.py
@@ -1,11 +1,7 @@
 import numpy as np
 import pandas as pd
-
-matriz = np.array([[1, 41, 71],
-                   [2, 51, 81],
-                   [3, 61, 91]]).astype(np.float32)
-
-
-
-matriz[1, 1]=777.4
-print(matriz)
+a=pd.DataFrame()
+for i in range(5):
+    a.append([pd.Series(i)])
+    
+print (a.T)
