5ab88812eda025da2bd523606cf3121be3bd9af8
wfasolo <wfasolo@gmail.com>
2021-03-19 09:59:25 -0300

    lab

--- outros/estufa/estufa.ino
+++ outros/estufa/estufa.ino
@@ -1,12 +1,15 @@
+//https://4.bp.blogspot.com/-8ez2w_os0sA/WudzMs5oOWI/AAAAAAAAChU/Hsgg7Yf9vNou9NRjWW4LsFoFuzvZ9lI2gCLcBGAs/s1600/7.png
+//esptool.py --chip ESP8266 --port /dev/ttyUSB1 --baud 115200 --before default_reset --after hard_reset  write_flash --flash_mode dio --flash_freq 40m --flash_size 4MB-c1  --no-compress 0x0 est.bin
+
 //sudo chmod 666 /dev/ttyUSB0
 
-#include <OneWire.h> //BIBLIOTECA NECESSÁRIA PARA O DS18B20
+#include <OneWire.h>           //BIBLIOTECA NECESSÁRIA PARA O DS18B20
 #include <DallasTemperature.h> //BIBLIOTECA NECESSÁRIA PARA O DS18B20
 
-#define DS18B20 A0
+#define DS18B20 4
 #define saida 12
 
-OneWire ourWire(DS18B20);//CONFIGURA UMA INSTÂNCIA ONEWIRE PARA SE COMUNICAR COM DS18B20
+OneWire ourWire(DS18B20);            //CONFIGURA UMA INSTÂNCIA ONEWIRE PARA SE COMUNICAR COM DS18B20
 DallasTemperature sensors(&ourWire); //PASSA A TEMPERATURA PARA O DallasTemperature
 
 #include <ThingerESP8266.h>
@@ -17,35 +20,43 @@ DallasTemperature sensors(&ourWire); //PASSA A TEMPERATURA PARA O DallasTemperat
 
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 
-float kp = 0.05,
-      ki = 0.05,
-      kd = 0.005,
+float kp = 250,
+      ki = 50,
+      kd = 200,
       p = 0,
       i = 0,
       d = 0,
       pid = 0,
       erro = 0,
-      tempo = 1,
-      dt = 0,
+      tempo = 99,
+      t_env = 0,
+      dt = 5,
+      temp_ant = 25,
+      temp = 30,
       valor = 0,
-      temp = 0,
-      pwm = 110,
-      leitura = 1,
-      setpoint = 30.5;
+      pwm = 10,
+      setpoint = 35;
 
 void setup()
 {
-
+  pinMode(LED_BUILTIN, OUTPUT);
   analogWrite(saida, pwm);
+  delay(10000);
+  pwm = 500;
+  yield();
+
   sensors.begin(); // INICIA O SENSOR DS18B20
 
   thing.add_wifi("LAB", "@@lucas@@");
 
-  thing["Parametros"] >> [](pson &out) {
+  thing["Parametros"] >> [](pson & out) {
     out["Temp"] = temp;
     out["PWM"] = pwm;
+    out["P"] = p;
+    out["I"] = i;
+    out["D"] = d;
   };
-  thing["PID"] << [](pson &in) {
+  thing["PID"] << [](pson & in) {
     if (in.is_empty())
     {
       in["Setpoint"] = setpoint;
@@ -65,62 +76,85 @@ void setup()
 
 void loop()
 {
-  controle();
+  temp = media();
+
+  if (millis() / 1000 - t_env >= 9 || abs(temp - temp_ant) >= 0.05)
+  {
+    t_env = millis() / 1000;
+    {
+      temp_ant = temp;
+      controle();
+      testePWM();
+      enviar();
+      yield();
+      digitalWrite(LED_BUILTIN, LOW);
+
+    }
+  }
+  analogWrite(saida, pwm);
+  delay(100);
+  yield();
+  digitalWrite(LED_BUILTIN, HIGH);
+
+
 }
 
 void enviar()
 {
   // enviar dados
   thing.stream(thing["Parametros"]);
-  thing.stream(thing["PID"]);
   thing.handle();
-  yield();
 }
 
 // --- Função média ---
-int media()
-{
+float media()
 
-  for (int x = 0; x < 50; x++)
-  {sensors.requestTemperatures();//REQUISITA A TEMPERATURA DO SENSOR
-    leitura += sensors.getTempCByIndex(0);
-    delay(5);
+{
+  int cm = 0;
+  valor = 0;
+
+
+  for (int im = 0; im <= 10; im++)
+  {
+    sensors.requestTemperatures(); //REQUISITA A TEMPERATURA DO SENSOR
+    float tp = sensors.getTempCByIndex(0);
+    if (tp != -127)
+    {
+      valor += tp;
+      cm++;
+    }
+    delay(10);
   }
-  leitura = leitura / 50;
-  return leitura;
+  valor = valor / cm;
+  yield();
+  return valor;
+}
+
+// --- Funcao teste do PWM ---
+float testePWM()
+{
+  if (pwm >= 1022)
+  {
+    pwm = 900;
+    p, i, d = 0;
+  }
+  else if (pwm <= 0)
+  {
+    pwm = 100;
+    p, i, d = 0;
+  }
 }
 
 // --- Função controle ---
 void controle()
 {
+  erro = ((setpoint - temp) / (setpoint));
+  dt = (millis() - tempo) / 1000;
+  tempo = millis();
+  p = (setpoint * kp * erro) / 100;
+  i = (setpoint * (ki * erro) * dt) / 100;
+  d += (setpoint * (kd * erro) / dt) / 100;
+  pid = p + i + d;
 
-  if ((millis() - tempo) >= 1000 * (5 - (4.99 * aberro)))
-  {
-    float valor_ant = valor;
-    float aberro = abs(erro);
-    valor = media();
-
-    erro = ((setpoint - valor_ant) / (setpoint + valor));
-    dt = (millis() - tempo) / 1000;
-    tempo = millis();
-    p = setpoint * kp * erro;
-    i = setpoint * (ki * erro) * dt;
-    d += setpoint * (kd * erro) / dt;
-    pid = p + i + d;
-
-    pwm = pwm - pid, 0;
-
-    if (pwm >= 1022)
-    {
-      pwm = 1022;
-    }
-    else if (pwm <= 0)
-    {
-      pwm = 1;
-    }
-
-    analogWrite(saida, pwm);
-    enviar();
-    yield();
-  }
+  pwm = pwm - pid, 0;
 }
---
+++ outros/estufa3/estufa3.ino
@@ -0,0 +1,167 @@
+//https://4.bp.blogspot.com/-8ez2w_os0sA/WudzMs5oOWI/AAAAAAAAChU/Hsgg7Yf9vNou9NRjWW4LsFoFuzvZ9lI2gCLcBGAs/s1600/7.png
+//esptool.py --chip ESP8266 --port /dev/ttyUSB1 --baud 115200 --before default_reset --after hard_reset  write_flash --flash_mode dio --flash_freq 40m --flash_size 4MB-c1  --no-compress 0x0 est.bin
+
+//sudo chmod 666 /dev/ttyUSB0
+
+#include <OneWire.h> //BIBLIOTECA NECESSÁRIA PARA O DS18B20
+#include <DallasTemperature.h> //BIBLIOTECA NECESSÁRIA PARA O DS18B20
+
+#define DS18B20 4
+#define saida 12
+
+OneWire ourWire(DS18B20);//CONFIGURA UMA INSTÂNCIA ONEWIRE PARA SE COMUNICAR COM DS18B20
+DallasTemperature sensors(&ourWire); //PASSA A TEMPERATURA PARA O DallasTemperature
+
+#include <ThingerESP8266.h>
+
+#define USERNAME "wfasolo"
+#define DEVICE_ID "Estufa"
+#define DEVICE_CREDENTIAL "@#lucas"
+
+float kp = 20, ki = 50, kd = 70,
+      p = 0, i = 0, d = 0,
+      integral = 0, derivada = 0,
+      pwm = 1020,  erro = 0, pid = 0,
+      tempo = 0, tempo1 = 0, tempo2 = 0,
+      temp = 27, temp_ant = 27,
+      setpoint = 27, sp_a = setpoint;
+
+
+
+ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  thing.add_wifi("LAB", "@@lucas@@");
+  sensors.begin(); // INICIA O SENSOR DS18B20
+
+  medir();
+  temp_ant = sp_a = temp;
+
+  thing["Parametros"] >> [](pson & out) {
+    out["Temp"] = temp;
+    out["PWM"] = int(pwm);
+    out["SetPoint"] = setpoint;
+    out["P"] = p;
+    out["I"] = i;
+    out["D"] = d;
+  };
+
+  thing["PID"] << [](pson & in) {
+    if (in.is_empty())
+    {
+      in["Setpoint"] = setpoint;
+      in["KP"] = kp;
+      in["KI"] = ki;
+      in["KD"] = kd;
+    }
+    else
+    {
+      setpoint = in["Setpoint"];
+      kp = in["KP"];
+      ki = in["KI"];
+      kd = in["KD"];
+    }
+  };
+}
+
+void loop()
+{
+  thing.handle();
+  digitalWrite(LED_BUILTIN, HIGH);
+  if (millis() - tempo1 >= 1000)
+  {
+    tempo1 = millis();
+    medir();
+  }
+
+  if (millis() - tempo2 >= 10000 || abs(temp - temp_ant) >= 0.05)
+  { tempo2 = millis();
+    temp_ant = temp;
+    calcular();
+    checar();
+  }
+}
+
+//--- checar as restricoes ---//
+void checar()
+{
+  //
+  if (setpoint != sp_a)
+  {
+    p = i = d = 0;
+    integral = derivada = 0;
+    sp_a = setpoint;
+    pwm=500;
+  }
+
+  //
+  if (temp > (setpoint - 1) && temp < (setpoint + 1))
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+
+  if (temp <= (setpoint - 5))
+  {
+    pwm = pwm / 2;
+  }
+  if (temp >= (setpoint + 5))
+  {
+    pwm = pwm * 2;
+
+  }
+
+  //
+  if (pwm >= 1022)
+  {
+    pwm = 1010;
+  }
+  else if (pwm <= 0)
+  {
+    pwm = 10;
+  }
+
+  analogWrite(saida, int(pwm));
+
+}
+
+//--- medir temepratura ---//
+void medir()
+{
+  int cm = 1;
+
+  for (int im = 0; im <= 10; im++) {
+    sensors.requestTemperatures();//REQUISITA A TEMPERATURA DO SENSOR
+    float tp = sensors.getTempCByIndex(0);
+    if (tp !=  -127) {
+      temp += tp;
+      cm++;
+    }
+    delay(5);
+
+  }
+  yield();
+  temp = temp / cm;
+}
+
+//--- Calcular PWM ---//
+void calcular()
+{
+  float prev_erro = erro;
+  erro =  setpoint - temp;
+
+  float dt = (millis() - tempo) / 1000;
+  tempo = millis();
+
+  integral = integral + erro * dt;
+  derivada = (erro - prev_erro) / dt;
+
+  p = setpoint * kp * erro * 0.1;
+  i = ki * integral * 0.001;
+  d = setpoint * kd *  derivada ;
+  pid = p + i + d;
+
+  pwm = pwm - pid, 0;
+}
