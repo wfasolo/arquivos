a64756c3e537c511c0d053531482c8a841dac9f2
wfasolo <wfasolo@gmail.com>
2021-07-05 18:14:03 -0300

    casa

--- trader/acoes Data.py
+++ trader/acoes Data.py
@@ -1,6 +1,9 @@
 import yfinance as yf
 import pandas as pd
+import numpy as np
 import plotly.graph_objects as go
+from sklearn import metrics
+from sklearn.svm import SVC
 
 dados = yf.download(  # or pdr.get_data_yahoo(...
     # tickers list or string as well
@@ -9,12 +12,12 @@ dados = yf.download(  # or pdr.get_data_yahoo(...
     # use "period" instead of start/end
     # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
     # (optional, default is '1mo')
-    period="5d",
+    period="1d",
 
     # fetch data by interval (including intraday if period < 60 days)
     # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
     # (optional, default is '1d')
-    interval="5m",
+    interval="15m",
 
     # group by ticker (to access via data['SPY'])
     # (optional, default is 'column')
@@ -38,28 +41,12 @@ dados = yf.download(  # or pdr.get_data_yahoo(...
 )
 
 df = pd.DataFrame(dados["PETR4.SA"])
-df=df.dropna()
+df = df.dropna()
 dd = pd.to_datetime((df.index).values, format="%Y-%d-%m %H:%M:%S")
 dd = (dd.strftime('%H:%M %d-%m-%y'))
 dd = pd.to_datetime(dd.values, format="%H:%M %d-%m-%y")
 print(dd)
-tabela = pd.DataFrame([dd,df.Open,df.Close,df.Low,df.High], index=['Data','Open','Close','Low','High']).T
+tabela = pd.DataFrame([dd, df.Open, df.Close, df.Low, df.High], index=[
+                      'Data', 'Open', 'Close', 'Low', 'High']).T
 print(tabela)
 
-
-trace1 = {
-    'x': tabela.Data,
-    'open': tabela.Open,
-    'close': tabela.Close,
-    'high': tabela.High,
-    'low': tabela.Low,
-    'type': 'candlestick',
-    
-    'showlegend': False
-}
-
-data = [trace1]
-layout = go.Layout()
-
-fig = go.Figure(data=data, layout=layout)
-fig.show()
