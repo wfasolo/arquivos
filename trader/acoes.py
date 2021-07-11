a2499e58863fbcb81e95b6da4f0b5ec6535c7ded
Your Name <wfasolo@gmail.com>
2021-07-11 13:39:11 -0300

    lab

--- trader/acoes.py
+++ trader/acoes.py
@@ -9,12 +9,12 @@ dados = yf.download(  # or pdr.get_data_yahoo(...
     # use "period" instead of start/end
     # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
     # (optional, default is '1mo')
-    period="2d",
+    period="5d",
 
     # fetch data by interval (including intraday if period < 60 days)
     # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
     # (optional, default is '1d')
-    interval="30m",
+    interval="1d",
 
     # group by ticker (to access via data['SPY'])
     # (optional, default is 'column')
@@ -61,4 +61,3 @@ layout = go.Layout()
 
 fig = go.Figure(data=data, layout=layout)
 fig.show()
-
--- trader/novo/KNR2.py
+++ trader/novo/KNR2.py
@@ -1,33 +1,44 @@
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
-import numpy as np 
+import numpy as np
 import DADOS2
 import RECALC
 import GRAF2
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+emp = input("Empresa 'PETR4.SA': ") or "PETR4.SA"
+per = input("Periodo '15 dias': ") or "15d"
+inter = input("Intervalo '1 dia': ") or "1d"
 
 previsao2 = []
 
-dados = RECALC.REC(DADOS2.TAB()['tabela'].drop(['Data', 'Hora'], axis=1)/1000)
+dados = DADOS2.TAB(emp, per, inter)[
+    'tabela'].drop(['Data', 'Hora'], axis=1)/1000
+
+dados_rec = RECALC.REC(dados)
 
 
-def TREINO(dados):
+def TREINO(dados_T):
     model1 = KNeighborsRegressor(n_neighbors=2)
-    model1.fit(dados['X_train'], dados['y_train'])
+    model1.fit(dados_T['X_train'], dados_T['y_train'])
 
     # Fazer previsoes
-    previsao = model1.predict(dados['ultimo'])
+    previsao = model1.predict(dados_T['ultimo'])
 
     return previsao
 
 
-for i in range(15):
-    previsao = TREINO(dados)
+for i in range(5):
+    previsao = TREINO(dados_rec)
     previsao2.extend(previsao)
+    print(dados)
+    previsao = pd.DataFrame(previsao, columns=[
+        'Open', 'Close', 'High', 'Low'])
+    dados = dados.append(previsao, ignore_index=True)
 
-    dados = pd.DataFrame(np.append(dados['X_train'],previsao,axis= 0))
-    dados=RECALC.REC(dados)  
-
+    dados_rec = RECALC.REC(dados)
 
 
 prev = pd.DataFrame(previsao2, columns=[
